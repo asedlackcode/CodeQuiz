c86ce229f68039b94865f3359c955df4e259b600
asedlack <asedlackcode@gmail.com>
2020-03-09 18:31:47 -0700

    update 2

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // create variables for start button and answers
 // var for highscore button
 // var for timer
+
+
+
+
 $("#beginQuiz").on("click", startTest);
     
 
 function startTest() {
-    console.log("started");
+    $("#startScreen").hide();
+    $("#main").removeClass("hide");
 };
+
